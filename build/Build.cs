2194f389cada930d80cf31276939780fa18fe29c
Anatolii Klots <anatolii.klots@SkyKick.com>
2021-05-12 15:16:56 +0200

    minor changes

--- build/Build.cs
+++ build/Build.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
