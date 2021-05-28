9a6d3b891e30b3dd281adb6d4abf7c914532e4e7
Anatolii Klots <anatolii.klots@SkyKick.com>
2021-05-28 14:36:45 +0200

    Adding a project for http-related extensions

---
+++ src/DotNet.Cookbook.Http/HttpResponseMessageExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotNet.Cookbook.Http
+{
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// Adds response's content to the message of <see cref="HttpRequestException"/> for InternalServerError http status code if any.
+        /// </summary>
+        /// <exception cref="HttpRequestException">When <see cref="HttpResponseMessage.StatusCode"/> is not successful.</exception>
+        public static async Task<HttpResponseMessage> EnsureSuccessStatusCodeAsync(this HttpResponseMessage response)
+        {
+            if (response.StatusCode == HttpStatusCode.InternalServerError &&
+                response.Content.Headers.ContentLength > 0)
+            {
+                var serviceMessage = await response.Content
+                    .ReadAsStringAsync()
+                    .ConfigureAwait(false);
+
+                if (!string.IsNullOrEmpty(serviceMessage))
+                {
+                    throw new HttpRequestException(string.Format(
+                        System.Globalization.CultureInfo.InvariantCulture,
+                        "Response status code does not indicate success: {0} ({1}).{2}",
+                        (int)response.StatusCode,
+                        response.ReasonPhrase,
+                        serviceMessage));
+                }
+            }
+
+            return response
+                .EnsureSuccessStatusCode();
+        }
+    }
+}
