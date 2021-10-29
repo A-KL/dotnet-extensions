f91eee4d61d961b9a1118b14ff5ddfd7eeb993f5
Anatolii Klots <anatolii.klots@SkyKick.com>
2021-10-29 18:07:28 +0200

    Adding manager that allows to re-map keys in azure keyvault

---
+++ src/DotNet.Cookbook.KeyVault/DelegatedKeyVaultSecretManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Security.KeyVault.Secrets;
+
+namespace DotNet.Cookbook.KeyVault
+{
+    public class DelegatedKeyVaultSecretManager : KeyVaultSecretManager
+    {
+        private readonly IDictionary<string, string> _mapping;
+
+        public DelegatedKeyVaultSecretManager(IDictionary<string, string> map)
+        {
+            _mapping = map ?? throw new ArgumentNullException(nameof(map));
+        }
+
+        public override string GetKey(KeyVaultSecret secret)
+        {
+            if (_mapping.ContainsKey(secret.Name))
+            {
+                return _mapping[secret.Name];
+            }
+
+            return base.GetKey(secret);
+        }
+    }
+}
