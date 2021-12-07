441b27cbb3984648d97e76ca422d526cc509d259
Anatolii Klots <anatolii.klots@SkyKick.com>
2021-12-07 10:22:30 +0100

    adding extension for convenience

---
+++ src/DotNet.Cookbook.KeyVault/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNet.Cookbook.KeyVault
+{
+    public static class Extensions
+    {
+        public static IConfigurationBuilder AddAzureKeyVaultForUri(this IConfigurationBuilder configurationBuilder, Uri uri, IDictionary<string, string> mapping)
+        {
+            return configurationBuilder.AddAzureKeyVault(
+                    uri,
+                    new DefaultAzureCredential(),
+                    new DelegatedKeyVaultSecretManager(mapping));
+        }
+    }
+}
