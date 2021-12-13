7f555e01b2eaac8992084345fab3a8ad1e40ef50
Anatolii Klots <anatolii.klots@SkyKick.com>
2021-12-13 15:22:46 +0100

    adding filters and KV

---
+++ src/DotNet.Cookbook.Filters/ValidateRequiredPramsAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DotNet.Cookbook.Filters
+{
+    /// <summary>
+    /// Validate that the <see cref="HttpRequest"/> contains relevant query parameter(s) marked as <see cref="BindRequiredAttribute"/> or <see cref="RequiredAttribute"/>.
+    /// </summary>
+    public class ValidateRequiredPramsAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var expected = string.Join(", ", context.ModelState.Keys);
+                var received = context.HttpContext.Request.QueryString.Value;
+
+                context.Result = new BadRequestObjectResult($"Missing or invalid query param(s). Expected: `{expected}` Param(s) received: '{received}'");
+            }
+        }
+    }
+}
